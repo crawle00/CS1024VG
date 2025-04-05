7e60eaa8e5c7c28ca426bdee2f6d98821184d026
Christian Rawley <christianrawley0@gmail.com>
2025-04-05 14:59:26 -0500

    tile size & paint components

--- CS1024VG/src/GamePanel.java
+++ CS1024VG/src/GamePanel.java
@@ -1,14 +1,34 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements Runnable {
 	public Thread thread;
 	
+	final int tileSize = 16 * 3; // 16x16 and scale of 3
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 9;
+	
 	public GamePanel() {
 		thread = new Thread();
+		this.setPreferredSize(new Dimension(maxScreenCol*tileSize, maxScreenRow*tileSize));
 	}
 	
 	public void start() {
 		thread.start();
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		Toolkit.getDefaultToolkit().sync();
 	}
 	
 	@Override
--- CS1024VG/src/Main.java
+++ CS1024VG/src/Main.java
@@ -1,5 +1,3 @@
-import java.awt.Dimension;
-
 import javax.swing.JFrame;
 
 public class Main {
@@ -8,9 +6,9 @@ public class Main {
 		GamePanel game = new GamePanel();
 		
 		frame.add(game);
+		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-		frame.setMinimumSize(new Dimension(960,540));
 		frame.setResizable(false);
 		frame.setVisible(true);
 		
