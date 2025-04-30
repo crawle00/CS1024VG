c44e54aecc47d0f457af907ad00a8d6da5a2e55e
PC <PC@Elijahs-Desktop>
2025-04-29 19:57:55 -0500

    boss is shooting but a bunch of errors

--- CS1024VG/src/Boss.java
+++ CS1024VG/src/Boss.java
@@ -1,76 +1,89 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import javax.imageio.ImageIO;
 
 public class Boss {
-	public int bossHealth = 100;
-	
-	public int bossx = 0;
-	public int bossy = 0;
-	BufferedImage phase1, phase2, phase3;
-	BossProjectile b = new BossProjectile();
-	
-	public Boss(){
-	}
-	public Boss(int bossHealth, int bossx, int bossy) {
-		this.bossHealth = bossHealth;
-		this.bossx = bossx;
-		this.bossy = bossy;
-		try {
-		phase1 = ImageIO.read(getClass().getResourceAsStream("/Boss/bossPhase1.png"));
-		phase2 = ImageIO.read(getClass().getResourceAsStream("/Boss/bossPhase2.png"));
-		phase3 = ImageIO.read(getClass().getResourceAsStream("/Boss/bossPhase3.png"));
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void drawBoss(Graphics2D g2) {
-		BufferedImage image = null;
-		
-		if(this.bossHealth >= 70) {
-			image = phase1;
-		}
-		else if(this.bossHealth >= 30) {
-			image = phase2;
-		}
-		else if(this.bossHealth < 30 && this.bossHealth > 0) {
-			image = phase3;
-		}
-		else {
-			//Thien make a you win page
-			System.out.println("You Win!");
-		}
-		
-		
-		
-		g2.drawImage(image, bossx, bossy, 100, 100, null);
-}
-	
-	
-	public void update() {
-			
-		
-	}
-	public int getBossHealth() {
-		return bossHealth;
-	}
-	public void setBossHealth(int bossHealth) {
-		this.bossHealth = bossHealth;
-	}
-	public int getBossx() {
-		return bossx;
-	}
-	public void setBossx(int bossx) {
-		this.bossx = bossx;
-	}
-	public int getBossy() {
-		return bossy;
-	}
-	public void setBossy(int bossy) {
-		this.bossy = bossy;
-	}
+    public int bossHealth = 100;
+    public int bossx = 0;
+    public int bossy = 0;
+
+    private BufferedImage phase1, phase2, phase3;
+    private GamePanel gp;
+    private Random rand = new Random();
+
+    private int shootCooldown = 0;
+    private final int shootInterval = 60; // 60 frames
+    private List<BossProjectile> projectiles = new ArrayList<>();
+
+    public Boss(GamePanel gp, int bossHealth, int bossx, int bossy) {
+        this.gp = gp;
+        this.bossHealth = bossHealth;
+        this.bossx = bossx;
+        this.bossy = bossy;
+
+        try {
+            phase1 = ImageIO.read(getClass().getResourceAsStream("/Boss/bossPhase1.png"));
+            phase2 = ImageIO.read(getClass().getResourceAsStream("/Boss/bossPhase2.png"));
+            phase3 = ImageIO.read(getClass().getResourceAsStream("/Boss/bossPhase3.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        shootCooldown--;
+        if (shootCooldown <= 0) {
+            shootPattern();
+            shootCooldown = shootInterval;
+        }
+
+        projectiles.removeIf(BossProjectile::isOffScreen);
+        for (BossProjectile p : projectiles) {
+            p.update();
+        }
+    }
+
+    public void drawBoss(Graphics2D g2) {
+        BufferedImage image = null;
+
+        if (bossHealth >= 70) {
+            image = phase1;
+        } else if (bossHealth >= 30) {
+            image = phase2;
+        } else if (bossHealth > 0) {
+            image = phase3;
+        } else {
+            System.out.println("You Win!");
+            return;
+        }
+
+        g2.drawImage(image, bossx, bossy, 100, 100, null);
+
+        for (BossProjectile p : projectiles) {
+            p.draw(g2);
+        }
+    }
+
+    private void shootPattern() {
+        String[] directions = {
+            "up", "down", "left", "right",
+            "upleft", "upright", "downleft", "downright"
+        };
+
+        for (String dir : directions) {
+            projectiles.add(new BossProjectile(dir, bossx, bossy, gp));
+        }
+    }
+
+    // Getter/setter methods
+    public int getBossHealth() { return bossHealth; }
+    public void setBossHealth(int bossHealth) { this.bossHealth = bossHealth; }
+    public int getBossx() { return bossx; }
+    public void setBossx(int bossx) { this.bossx = bossx; }
+    public int getBossy() { return bossy; }
+    public void setBossy(int bossy) { this.bossy = bossy; }
 }
--- CS1024VG/src/BossProjectile.java
+++ CS1024VG/src/BossProjectile.java
@@ -1,85 +1,51 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 
 public class BossProjectile extends Projectile {
+    private BufferedImage bpimage;
+    private String bpdirection;
+    private int bpx, bpy;
+    private int bpspeed = 2;
+    private GamePanel bpgp;
 
-	private BufferedImage bpimage;
-	private String bpdirection;
-	private int bpx;
-	private int bpy;
-	private int bpspeed = 2;
-	private GamePanel bpgp;
-	
-	
-	public BossProjectile() {}
-	
-	public BossProjectile(String direction, int x, int y, GamePanel gp) {
-		super(direction, x, y, gp);
-		try {
-			bpimage = ImageIO.read(getClass().getResourceAsStream("BossProjectile.png"));
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		
-		this.bpdirection = direction;
-		this.bpx = x + 16;
-		this.bpy = y + 32 + 16;
-		this.bpgp = gp;
-	}
-	
-	@Override
-	public boolean isOffScreen() {
-	    return bpx < 0 || bpx > bpgp.maxScreenCol * bpgp.tileSize ||
-	           bpy < 0 || bpy > bpgp.maxScreenRow * bpgp.tileSize;
-	}
-	
+    public BossProjectile(String direction, int x, int y, GamePanel gp) {
+        super(direction, x, y, gp);
+        this.bpdirection = direction;
+        this.bpx = x + 50; // Adjust projectile spawn point relative to boss
+        this.bpy = y + 50;
+        this.bpgp = gp;
 
-	@Override
-	public void update() {
-		if (bpdirection == "up") bpy -= bpspeed;
-		else if (bpdirection.equals("down")) bpy += bpspeed;
-		else if (bpdirection.equals("left")) bpx -= bpspeed;
-		else if (bpdirection.equals("right")) bpx += bpspeed;
-	}
-	
-	@Override
-	public void draw(Graphics2D g2) {
-		g2.drawImage(bpimage, bpx, bpy, bpgp.tileSize / 2, bpgp.tileSize / 2, null);
-	}
-	
-	public String getBpdirection() {
-		return bpdirection;
-	}
+        try {
+            bpimage = ImageIO.read(getClass().getResourceAsStream("/Boss/BossProjectile.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void setBpdirection(String bpdirection) {
-		this.bpdirection = bpdirection;
-	}
+    @Override
+    public boolean isOffScreen() {
+        return bpx < 0 || bpx > bpgp.maxScreenCol * bpgp.tileSize ||
+               bpy < 0 || bpy > bpgp.maxScreenRow * bpgp.tileSize;
+    }
 
-	public int getBpx() {
-		return bpx;
-	}
+    @Override
+    public void update() {
+        switch (bpdirection) {
+            case "up" -> bpy -= bpspeed;
+            case "down" -> bpy += bpspeed;
+            case "left" -> bpx -= bpspeed;
+            case "right" -> bpx += bpspeed;
+            case "upleft" -> { bpx -= bpspeed; bpy -= bpspeed; }
+            case "upright" -> { bpx += bpspeed; bpy -= bpspeed; }
+            case "downleft" -> { bpx -= bpspeed; bpy += bpspeed; }
+            case "downright" -> { bpx += bpspeed; bpy += bpspeed; }
+        }
+    }
 
-	public void setBpx(int bpx) {
-		this.bpx = bpx;
-	}
-
-	public int getBpy() {
-		return bpy;
-	}
-
-	public void setBpy(int bpy) {
-		this.bpy = bpy;
-	}
-
-	public int getBpspeed() {
-		return bpspeed;
-	}
-
-	public void setBpspeed(int bpspeed) {
-		this.bpspeed = bpspeed;
-	}
-
+    @Override
+    public void draw(Graphics2D g2) {
+        g2.drawImage(bpimage, bpx, bpy, bpgp.tileSize / 2, bpgp.tileSize / 2, null);
+    }
 }
