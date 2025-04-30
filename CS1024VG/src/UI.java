5a43c2bee0f50b081de97c9b6a9e1508d9e3b16e
thien <thien@10.95.167.116>
2025-04-29 20:03:23 -0500

    saving work before pulling

--- CS1024VG/src/UI.java
+++ CS1024VG/src/UI.java
@@ -1,4 +1,22 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class UI {
-
+	BufferedImage bossHPOutline, playerHPOutline, bossHPBar,playerHPBar,shootCD;
+	public UI(){
+		try {
+			bossHPOutline = ImageIO.read(getClass().getResourceAsStream("/UI/healthbar.png"));
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	public void drawBoss(Graphics2D g2) {
+		BufferedImage bossHPOutlineImage = null;
+		
+		bossHPOutlineImage = bossHPOutline;
+		
+		g2.drawImage(bossHPOutlineImage, 384, 100, 150, 75, null);
 }
